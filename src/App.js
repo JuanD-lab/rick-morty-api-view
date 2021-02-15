b82ba7e0c667b3c45ec231f2762349aed81385e8
JuanD-lab <yo-juan@outlook.com>
2021-02-15 18:05:32 -0500

    final.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import './App.css';
 import SearchBox from './components/SearchBox'
-import LocationContainer from './components/LocationContainer'
 
 function App() {
   return (
     <div className="App">
       <img src="./images/logo.png" alt=""/>
       <SearchBox/>
-      {/* <LocationContainer/> */}
     </div>
   );
 }
